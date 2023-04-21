fee87eb0ac0443fa5ef565e1c8f437ebb4034cc5
mohsinhasankhan <122450318+mohsinhasankhan@users.noreply.github.com>
2023-04-21 18:05:26 +0530

    added new files

---
+++ script.js
@@ -0,0 +1,19 @@
+//Hamburger
+let hamburger=document.querySelector(".hamburger")
+let dropdown=document.querySelector(".dropdown")
+let crossicon= document.querySelector(".close-icon")
+let openicon= document.querySelector(".open-icon")
+hamburger.addEventListener('click',()=>{
+    dropdown.classList.toggle('active')
+    crossicon.classList.toggle('active')    
+    if(dropdown.classList.contains('active')){
+        openicon.style.display="none"
+        crossicon.style.display="block"
+    }
+    else{
+        crossicon.style.display="none"
+        openicon.style.display="block"
+
+    }
+
+})
